8f2e88c62f798085bd21a825f1eaae7ca07313e2
AZER SALANDA <azer.salanda@gmail.com>
2020-12-10 11:05:00 +0800

    installs

---
+++ backend/index.js
@@ -0,0 +1,4 @@
+const Web3 = require('web3');
+const web3 = new Web3(new Web3.providers.HttpProvider('https://k0kw6ia9j2:Nlk6vjccXvm3Z1_F4WXuWq5bR4YLgeegi3UT3JsHLww@k0jy73d24v-k0pu9805pa-rpc.kr0-aws.kaleido.io/'));
+
+console.log(web3);
